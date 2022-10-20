5a7ed30fc526d71c064dc4fd4eb1010dad72a04f
normaple <115323025+normaple@users.noreply.github.com>
2022-10-20 14:42:44 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿bool loop = true;
+Console.WriteLine("Enter a number");
+double calc = Convert.ToDouble(Console.ReadLine());
+while (loop)
+{
+    Console.WriteLine("Enter an operator");
+    char math_operator = Convert.ToChar(Console.ReadLine());
+    if (math_operator == '*')
+    {
+        Console.WriteLine("Enter a number");
+        double number = Convert.ToDouble(Console.ReadLine());
+        calc *= number;
+    }
+    else if (math_operator == '/')
+    {
+        Console.WriteLine("Enter a number");
+        double number = Convert.ToDouble(Console.ReadLine());
+        calc /= number;
+    }
+    else if (math_operator == '+')
+    {
+        Console.WriteLine("Enter a number");
+        double number = Convert.ToDouble(Console.ReadLine());
+        calc += number;
+    }
+    else if (math_operator == '-')
+    {
+        Console.WriteLine("Enter a number");
+        double number = Convert.ToDouble(Console.ReadLine());
+        calc -= number;
+    }
+    else if (math_operator == '=')
+    {
+        Console.WriteLine(calc);
+    } else if (math_operator == 'c')
+    {
+        calc = 0;
+        Console.WriteLine("Enter a number");
+        calc = Convert.ToDouble(Console.ReadLine());
+    } else if (math_operator == '0')
+    {
+        loop = false;
+    } else
+    {
+        Console.WriteLine("Error");
+        Console.WriteLine("Enter a number");
+        calc = Convert.ToDouble(Console.ReadLine());
+    }
+
+     
+
+} 
+    
+    
+    
+
+
+
+
